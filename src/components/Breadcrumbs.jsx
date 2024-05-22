20b7336ff5c0f0592fa18596cec1b2e930f29927
akiko0311 <119967183+chanhtima@users.noreply.github.com>
2024-05-22 09:47:38 +0700

    add Breadcrumbs;

---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import HomeIcon from "../assets/icon/HomIcom.svg";
+function Breadcrumbs({ page_url, page_name, sub_page, title_name }) {
+  return (
+    <div className="px-12 md:px-24  xl:px-40">
+      <div className="breadcrumbs justify-end md:flex items-center py-8">
+        <ul>
+          <li>
+            <a href="/">
+              <img src={HomeIcon} alt="" />
+              Home
+            </a>
+          </li>
+          <li>
+            <span className="px-2">/</span>
+            <a href={page_url}>{title_name}</a>
+          </li>
+          {sub_page && (
+            <li className=" max-md:!hidden">
+              <span className="px-2">/</span>
+              <a>{sub_page}</a>
+            </li>
+          )}
+        </ul>
+      </div>
+      <div className=" flex items-center">
+        <h1 className="text-[34px] pr-4">{page_name}</h1>
+        <span className="border-b-4  border-black w-[70%] xl:w-[90%]  max-md:hidden"></span>
+      </div>
+    </div>
+  );
+}
+
+export default Breadcrumbs;
--- src/pages/Users/About.jsx
+++ src/pages/Users/About.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Breadcrumbs from "../../components/Breadcrumbs";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <Breadcrumbs
+        page_name="About us"
+        page_url="/about"
+        sub_page="Front-End Developer"
+        title_name="About us"
+      />
+      <div className="section-container min-h-screen"></div>
+    </>
+  );
 }
 
-export default About
+export default About;
